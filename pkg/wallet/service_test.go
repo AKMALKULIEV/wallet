30039cb05be338c7ca900347b063b0715fabfedf
Akmal <akmal@go.alif.academy>
2021-04-11 19:46:48 +0500

    modified

--- pkg/wallet/service_test.go
+++ pkg/wallet/service_test.go
@@ -3,16 +3,15 @@ package wallet
 import (
 	// "reflect"
 	"testing"
-
 	// "github.com/AKMALKULIEV/wallet/pkg/types"
 )
 
 func TestService_FindAccoundById_Method_NotFound(t *testing.T) {
 	svc := Service{}
-	svc.RegisterAccount("+9920000001")
+	svc.RegisterAccount("654679654646")
 
 	account, err := svc.FindAccountByID(46)
 	if err == nil {
-		t.Errorf("\ngot > %v \nwant > nil", account)
+		t.Errorf("got > %v want > nil", account)
 	}
 }
